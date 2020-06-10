8eade64169e7a07dbaa70288e02bd457fa78983d
baiduh <cauecpl@gmail.com>
2020-06-10 16:42:16 -0300

    declarações e lexico

---
+++ curso JS/função/declaracoes.js
@@ -0,0 +1,14 @@
+// declaration
+function soma(x, y) {
+    return x + y
+}
+
+// expression
+const sub = function(x, y) {
+    return x - y
+} 
+
+// named expressesion
+const multi = function multi(x, y) {
+    return x * y
+}
