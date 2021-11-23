7de48541d17cd9859407214e912cc484447c6797
Cauê <cauecpl@gmail.com>
2021-11-23 20:16:38 -0300

    while

--- Front-End/JS/fundamentos/variaveisEmLooping.js
+++ Front-End/JS/fundamentos/variaveisEmLooping.js
@@ -25,3 +25,33 @@ for(let z = 0; z<10; z++) {
 
 //console.log(funcslet[2])
 funcslet[9]
+
+// While
+const destinos = new Array(
+    `Salvador`,
+    `Rio de Janeiro`,
+    `São Paulo`
+);
+
+console.log('Destinos possíveis:');
+console.log(destinos);
+
+const idade = 18;
+const acompanhado = false;
+const temPassagem = true;
+const destinoPassagem = 'São Paulo';
+let contador = 0;
+
+// Comprando passagem
+
+const podeComprar = idade >= 18 || acompanhado == true;
+
+while(destinos[contador] != destinoPassagem && contador < destinos.length){
+    contador++;
+};
+
+if(contador >= destinos.length){
+    console.log(`\nO destino ${destinoPassagem} não está disponível`)
+} else {
+    console.log(`\n${destinos[contador]} está disponível!`);
+}
