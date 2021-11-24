4f7a69802056a0b5d8bab453a8eb0dae8871a82e
Cauê <cauecpl@gmail.com>
2021-11-23 22:54:21 -0300

    compra e embarque

--- Front-End/JS/fundamentos/variaveisEmLooping.js
+++ Front-End/JS/fundamentos/variaveisEmLooping.js
@@ -1,31 +1,3 @@
-const funcs = []
-
-for(var i = 0; i<10; i++){
-    funcs.push(function(){
-        console.log(i)
-    })
-}
-
-funcs[2] ()
-console.log(' ')
-
-const funcslet = []
-
-
-for(let z = 0; z<10; z++) {
-    funcslet.push(function(){
-        console.log(z)
-    })
-}
-
-
-/* for(let z = 0; z<10; z++) {
-    funcslet.push( console.log(z) )
-} */
-
-//console.log(funcslet[2])
-funcslet[9]
-
 // While
 const destinos = new Array(
     `Salvador`,
@@ -33,25 +5,49 @@ const destinos = new Array(
     `São Paulo`
 );
 
-console.log('Destinos possíveis:');
-console.log(destinos);
+console.log('Destinos possíveis:', destinos);
 
 const idade = 18;
-const acompanhado = false;
-const temPassagem = true;
+const acompanhado = true;
+let temPassagem = false;
 const destinoPassagem = 'São Paulo';
 let contador = 0;
 
-// Comprando passagem
-
+// Verificando possibilidade de compra de passagem
 const podeComprar = idade >= 18 || acompanhado == true;
 
+// verificando se destino existe
 while(destinos[contador] != destinoPassagem && contador < destinos.length){
     contador++;
 };
 
+let destinoExiste = false;
 if(contador >= destinos.length){
     console.log(`\nO destino ${destinoPassagem} não está disponível`)
 } else {
     console.log(`\n${destinos[contador]} está disponível!`);
+    destinoExiste = true;
 }
+
+// Executando compra de passagem
+if(podeComprar == true && destinoExiste == true){
+    destinos.splice(contador,1);
+    temPassagem = true;
+    console.log("Passagem comprada com sucesso.");
+}else{
+    console.log("Sua compra não foi efetuada.");
+};
+console.log("\nDestinos disponíveis:", destinos);
+
+// Verificação de embarque
+console.log('\nHora do embarque');
+const embarqueAcompanhado = false;
+if(temPassagem == true && idade >= 18) {
+    console.log('Boa viagem');
+}else if(temPassagem == true && idade < 18 && embarqueAcompanhado == true){
+    console.log('Boa viagem aos 2');
+}else if(temPassagem == true && idade <18 && embarqueAcompanhado == false){
+    console.log('Menor não pode embarcar sozinho')
+}else{
+    console.log('Proibido embarcar sem passagem');
+};
